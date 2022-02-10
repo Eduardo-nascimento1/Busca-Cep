8baef047eabfe1f7a1ab045d939de919797d8f90
Eduardo-nascimento1 <eduardo1programador@hotmail.com>
2022-02-10 20:44:42 -0300

    adicionando o css e deixando responsivo

--- src/components/Buscacep.js
+++ src/components/Buscacep.js
@@ -1,15 +1,26 @@
 import React from "react";
 import './Buscacep.css'
+import {FiSearch} from "react-icons/fi"
 function Buscacep() {
     return (
         <div className="Container">
-            <h1 className="title"> Buscador de Cep</h1>
+            <h1 className="title"> Buscador CEP</h1>
 
             <div className="containerInput">
                 <input type="text" 
                 placeholder="Digite seu cep..."/>
-                
+            <button type="button" className="buttonsearch">
+                <FiSearch size={25} color="#fff"/>
+            </button>
             </div>
+            <main className="main">
+                <h2>CEP: 79003014</h2>
+                
+                <span> Rua teste algum</span>
+                <span> complemento: algum complemento</span>
+                <span> bairro Serra Verde </span>
+                <span>Minas Gerais - MG</span>
+            </main>
         </div>
     );
 }
